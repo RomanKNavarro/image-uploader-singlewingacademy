6843bac4727b55e7b77b76b85a31bee4a5545065
Roman Navarro <ronnoverro@gmail.com>
2022-12-24 16:04:59 -0800

    Success. npm run dev work

--- client/src/components/Regular/Regular.js
+++ client/src/components/Regular/Regular.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
+import '.Regular.scss';
+import LoadingDots from 'imgs/loading-dots.gif';
 
 const Regular = () => {
+  const [file, setFile] = useState(null);
+  const [inputContainsFile, setInputContainsFile] = useState(false);
+  const [currentlyUploading, setCurrentlyUploading] = useState(false);
+  const [imageId, setImageId] = useState(null);
+  const [progress, setProgress] = useState(null);     // SUPER STRAIGHTFORWARD
+
+  // FUNCTIONS:
+  const handleFile = (event) => {
+    setFile(event.target.files[0]);   // FILE SET AS A STATE VALUE
+    setInputContainsFile(true);       
+  };
+
   return (
     <div>Regular</div>
   )
